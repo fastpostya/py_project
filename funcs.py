e5d622e14e9f2d65759447928202f99b6caa16aa
fastpostya <fastpost@rambler.ru>
2023-01-20 00:11:13 +0300

    add function diff

--- funcs.py
+++ funcs.py
@@ -1,5 +1,10 @@
-def funcs():
+def sum():
     n = float(input("Введите первое число: "))
     m = float(input("Введите второе число: "))
     print(f"Сумма чисел равна: {n + m}")
+
+def diff():
+    n = float(input("Введите первое число: "))
+    m = float(input("Введите второе число: "))
+    print(f"Разность чисел равна: {n + m}")
 
